937598c617946e14e6202b86841fb7422a1e047e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-02-07 06:44:19 -0300

    Update AboutMeApp.swift

--- app/src/main/swift/com/Bitguard/ui/AboutMeApp.swift
+++ app/src/main/swift/com/Bitguard/ui/AboutMeApp.swift
@@ -5,7 +5,7 @@ See the License.txt file for this sample’s licensing information.
 import SwiftUI
 
 @main
-struct AboutMeApp: App {
+struct AboutMeApp: Bitguard {
     var body: some Scene {
         WindowGroup {
             ContentView()
